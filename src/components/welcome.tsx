c975d9166ec0f4719f23edf0a6204945c2d38b72
Prisca Onyemaechi <priscaonyemaechi26@gmail.com>
2025-01-31 03:06:49 +0100

    Worked on the welcome section

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,32 +1,49 @@
 import { motion } from "motion/react";
-import './styles/welcome.css';
+import "./styles/welcome.css";
 
 function Welcome() {
   return (
     <>
-    <section id="welcome">
-    <div className="images">
-        <img src="src/assets/welcome.png" alt="logo" className="welcome-big" />
-        <img src="src/assets/student.png" alt="logo" className="welcome-small" />
-        <div className="students">
-          <p className="number">800+</p>
-          <p className="text">SATISFIED STUDENTS</p>
+      <section id="welcome">
+        <div className="images">
+          <img
+            src="src/assets/welcome.png"
+            alt="logo"
+            className="welcome-big"
+          />
+          <img
+            src="src/assets/student.png"
+            alt="logo"
+            className="welcome-small"
+          />
+          <div className="students">
+            <p className="number">800+</p>
+            <p className="text">SATISFIED STUDENTS</p>
+          </div>
         </div>
-      </div>
-      <div className="texts">
-        <span>Welcome to</span><h2>
-            My ICT Academy
-        </h2>
-        <p>
-            My ICT Academy is a leading online learning platform that helps anyone learn business, software, technology, and creative skills to achieve personal and professional goals. Through individual, corporate, academic and government subscriptions, members have access to the My ICT Academy video library of engaging, top-quality courses taught by recognized industry experts.
-        </p>
-        <motion.div
+        <div className="texts">
+          <span className="welcometo">Welcome to</span>
+          <h2>My ICT Academy</h2>
+          <div className="margin">
+            <div className="hr"></div>
+            <p>
+              My ICT Academy is a leading online learning platform that helps
+              anyone learn business, software, technology, and creative skills
+              to achieve personal and professional goals. Through individual,
+              corporate, academic and government subscriptions, members have
+              access to the My ICT Academy video library of engaging,
+              top-quality courses taught by recognized industry experts.
+            </p>
+            <div className="hr"></div>
+          </div>
+
+          <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 0.4 }}
           >
             <motion.button
-              className="animated-btn"
+              className="animated-button"
               whileHover="hover"
               initial="initial"
               variants={{
@@ -49,9 +66,8 @@ function Welcome() {
               <span className="button-text">Learn more</span>
             </motion.button>
           </motion.div>
-      </div>
-    </section>
-      
+        </div>
+      </section>
     </>
   );
 }
